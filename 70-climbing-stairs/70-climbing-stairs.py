0a69e0dfc1f1173af456383d7acc355c76126481
zekercode <101523974+zekercode@users.noreply.github.com>
2022-07-09 10:19:21 -0400

    Time: 42 ms (60.37%), Space: 13.9 MB (57.08%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one = 1
+        two = 1
+        
+        for _ in range(n-1):
+            temp = one
+            one += two
+            two = temp
+        return one
+        
+            
